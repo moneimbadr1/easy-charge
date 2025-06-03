27c853844564634a2a02d47b2d1f83e98e7aa6c0
MON3EM <131455349+moneimbadr1@users.noreply.github.com>
2025-06-03 16:28:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler
 async def start(update, context):
     await update.message.reply_text("مرحبا! أنا بوتك.")
 
-app = ApplicationBuilder().token('7902964725:AAFqOlXAQLabw3d8jWjs8TULRlFsYOMtBjY').build()
+app = ApplicationBuilder().token('').build()
 
 app.add_handler(CommandHandler("start", start))
 
