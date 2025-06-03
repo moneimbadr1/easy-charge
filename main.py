951f487c9298a15cfbb2d58c9859fe7597b25c9a
MON3EM <131455349+moneimbadr1@users.noreply.github.com>
2025-06-03 15:27:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-from flask import Flask, request
-import telegram
+from telegram.ext import ApplicationBuilder, CommandHandler
 
-app = Flask(__name__)
+async def start(update, context):
+    await update.message.reply_text("مرحبا! أنا بوتك.")
 
-TOKEN = "7902964725:AAFqOlXAQLabw3d8jWjs8TULRlFsYOMtBjY"
-bot = telegram.Bot(token=TOKEN)
+app = ApplicationBuilder().token('7902964725:AAFqOlXAQLabw3d8jWjs8TULRlFsYOMtBjY').build()
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    chat_id = update.message.chat.id
-    text = update.message.text
+app.add_handler(CommandHandler("start", start))
 
-    # هنا ترد على رسالة المستخدم
-    bot.send_message(chat_id=chat_id, text=f"استلمت رسالتك: {text}")
-    return 'OK'
-
-@app.route('/')
-def index():
-    return "بوت شغال"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+app.run_polling()
